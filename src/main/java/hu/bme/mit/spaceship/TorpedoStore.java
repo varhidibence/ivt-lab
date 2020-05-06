2ba34c951172c64992286f19fd61982b171be7fc
Bence <varhidibence93@gmail.com>
2020-05-06 08:37:56 +0200

    TorpedoStore fire method successful firing fixed

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -51,7 +51,7 @@ public class TorpedoStore {
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount =- numberOfTorpedos;
+      this.torpedoCount -= numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
